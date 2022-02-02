4b20d318813f324a6628bd6f41bca9394eb95ca1
aidancer55 <bluebonnets77@gmail.com>
2022-02-02 20:48:59 +0300

    Добавьте файлы проекта.

---
+++ 1.ChooseFile/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2.SumNumb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:/Autodesk";
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (string s in dirs)
+            {
+                Console.WriteLine(s);
+                string[] dirs1 = Directory.GetDirectories(s);
+                foreach (string s1 in dirs1)
+                {
+                    Console.WriteLine("\t" + s1);
+                }
+                string[] files1 = Directory.GetDirectories(s);
+                Console.WriteLine();
+                foreach (string sf1 in files1)
+                {
+                    Console.WriteLine("\t" + sf1);
+                }
+                Console.WriteLine();
+            }
+            string[] files = Directory.GetDirectories(path);
+            Console.WriteLine();
+            foreach (string sf in files)
+            {
+                Console.WriteLine(sf);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2. SumNumb/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2.SumNumb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "File";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);    
+            
+            }
+            string path1 = "File/Numbers.txt";
+            if (!File.Exists(path1))
+            {
+                File.Create(path1);
+            }
+            StreamWriter sw = new StreamWriter(path1);
+            int[] array = new int[10];
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+                sum += array[i];
+                sw.Write("{0} ", array[i]);
+            }
+            sw.Close();
+            Console.WriteLine("Сумма равна {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
